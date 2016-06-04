b3611d46f41195bb797f2e0fee558456bc7e9639
honeymon <ihoneymon@gmail.com>
2016-06-04 19:23:11 +0900

    Code fomatting

--- src/main/java/io/honeymon/springboot/slack/web/SlackSendController.java
+++ src/main/java/io/honeymon/springboot/slack/web/SlackSendController.java
@@ -5,11 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.honeymon.springboot.slack.integration.SlackNotifier;
-import io.honeymon.springboot.slack.integration.SlackNotifier.SlackMessage;
 import io.honeymon.springboot.slack.integration.SlackNotifier.SlackMessageAttachement;
 import io.honeymon.springboot.slack.integration.SlackNotifier.SlackTarget;
 
@@ -17,8 +15,8 @@ import io.honeymon.springboot.slack.integration.SlackNotifier.SlackTarget;
 public class SlackSendController {
 	@Autowired
 	private SlackNotifier slackNotifier;
-	
-	@RequestMapping(value="/", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public ResponseEntity<Boolean> send(@RequestBody SlackMessageAttachement message) {
 		return ResponseEntity.ok(slackNotifier.notify(SlackTarget.CH_INCOMING, message));
 	}
